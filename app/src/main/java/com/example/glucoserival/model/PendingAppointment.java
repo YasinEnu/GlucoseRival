37f884bd02f676a93718e91690fc7b93df6036c8
yasin.hosain <yasin.hosain@cloudwell.co>
2019-11-02 00:57:19 +0600

    merging with yasin branch

---
+++ app/src/main/java/com/example/glucoserival/model/PendingAppointment.java
@@ -0,0 +1,93 @@
+
+package com.example.glucoserival.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+@SuppressWarnings("unused")
+public class PendingAppointment implements Serializable {
+
+    @Expose
+    private String date;
+    @SerializedName("date_time")
+    private String dateTime;
+    @SerializedName("doctor_id")
+    private String doctorId;
+    @SerializedName("doctor_name")
+    private String doctorName;
+    @Expose
+    private String id;
+    @SerializedName("last_updated_date_time")
+    private Object lastUpdatedDateTime;
+    @Expose
+    private String status;
+    @SerializedName("user_id")
+    private String userId;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(String doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Object getLastUpdatedDateTime() {
+        return lastUpdatedDateTime;
+    }
+
+    public void setLastUpdatedDateTime(Object lastUpdatedDateTime) {
+        this.lastUpdatedDateTime = lastUpdatedDateTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+}
